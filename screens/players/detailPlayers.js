e0561c5a6ebcf1bdebc4cd798c448a47870b5ad2
Pramoda Piyumali <pramodapiyumali@gmail.com>
2023-03-22 22:15:58 +0530

    Edit players

--- screens/players/detailPlayers.js
+++ screens/players/detailPlayers.js
@@ -5,15 +5,39 @@ import { useNavigation } from "@react-navigation/native";
 
 const DetailPlayers = ({ route }) => {
   const playerRef = firebase.firestore().collection("players");
-  const [textHeading, onChangeHeadingText] = useState(route.params.item.heading);
+  const [textPName, onChangePNameText] = useState(route.params.item.pName);
+  const [textAge, onChangeAgeText] = useState(route.params.item.age);
+  const [textHeight, onChangeHeightText] = useState(route.params.item.height);
+  const [textFoot, onChangeFootText] = useState(route.params.item.foot);
+  const [textPosition, onChangePositionText] = useState(route.params.item.position);
+  const [textGoals, onChangeGoalsText] = useState(route.params.item.goals);
+  const [textAssists, onChangeAssistsText] = useState(route.params.item.assists);
+  const [textSheets, onChangeSheetsText] = useState(route.params.item.sheets);
+  const [textAttacking, onChangeAttackingText] = useState(route.params.item.attacking);
+  const [textDribbling, onChangeDribblingText] = useState(route.params.item.dribbling);
+  const [textDefending, onChangeDefendingText] = useState(route.params.item.defending);
+  const [textPassing, onChangePassingText] = useState(route.params.item.passing);
+  const [textPhysical, onChangePhysicalText] = useState(route.params.item.physical);
   const navigation = useNavigation();
 
   const updatePlayer = () => {
-    if (textHeading && textHeading.length > 0) {
+    if (textPName && textPName.length > 0) {
       playerRef
         .doc(route.params.item.id)
         .update({
-          heading: textHeading,
+          pName: textPName,
+          age:textAge,
+        height:textHeight,
+        foot:textFoot,
+        position:textPosition,
+        goals:textGoals,
+        assists:textAssists,
+        sheets:textSheets,
+        attacking:textAttacking,
+        dribbling:textDribbling,
+        defending:textDefending,
+        passing:textPassing,
+       physical:textPhysical,
         })
         .then(() => {
           navigation.navigate("PlayersHome");
@@ -26,12 +50,108 @@ const DetailPlayers = ({ route }) => {
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.textField}
-        onChangeText={onChangeHeadingText}
-        value={textHeading}
-        // placeholder="Update "
-      />
+     
+
+<Text style={styles.title}>Update Players</Text>
+
+<Text style={styles.title}>Personal Info</Text>
+
+<TextInput
+      style={styles.textField}
+      onChangeText={onChangePNameText}
+      value={textPName}
+      readOnly
+      
+    />
+
+<TextInput
+      style={styles.textField}
+      onChangeText={onChangeAgeText}
+      value={textAge}
+      
+      textContentType="number"
+    /> 
+
+<TextInput
+      style={styles.textField}
+      onChangeText={onChangeHeightText}
+      value={textHeight}
+      
+    /> 
+
+<TextInput
+      style={styles.textField}
+      onChangeText={onChangeFootText}
+      value={textFoot}
+      
+    />
+
+<TextInput
+      style={styles.textField}
+      onChangeText={onChangePositionText}
+      value={textPosition}
+      
+    />
+
+<Text style={styles.title}>Stats</Text>
+
+<TextInput
+              style={styles.textField}
+              onChangeText={onChangeGoalsText}
+              value={textGoals}
+              
+            />
+
+<TextInput
+              style={styles.textField}
+              onChangeText={onChangeAssistsText}
+              value={textAssists}
+              
+            />
+
+<TextInput
+              style={styles.textField}
+              onChangeText={onChangeSheetsText}
+              value={textSheets}
+              
+            />
+
+<Text style={styles.title}>Skills</Text>
+
+<TextInput
+              style={styles.textField}
+              onChangeText={onChangeAttackingText}
+              value={textAttacking}
+              
+            />
+
+<TextInput
+              style={styles.textField}
+              onChangeText={onChangeDefendingText}
+              value={textDefending}
+              
+            />
+
+<TextInput
+              style={styles.textField}
+              onChangeText={onChangeDribblingText}
+              value={textDribbling}
+              
+            />
+
+<TextInput
+              style={styles.textField}
+              onChangeText={onChangePassingText}
+              value={textPassing}
+              
+            />
+
+<TextInput
+              style={styles.textField}
+              onChangeText={onChangePhysicalText}
+              value={textPhysical}
+              
+            />
 
       <Pressable
         style={styles.buttonUpdate}
