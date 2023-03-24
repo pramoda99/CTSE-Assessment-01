d95b03fe01e767b030eaa673e1e9a618f9f465fd
Pawani Muthusala Perera <89269937+MuthusalaPerera@users.noreply.github.com>
2023-03-24 23:25:32 +0530

    new

--- screens/login.js
+++ screens/login.js
@@ -23,7 +23,7 @@ const Login = () => {
   };
   return (
     <View style={styles.container}>
-      <Text style={{ fontWeight: "bold", fontSize: 26 }}>Login</Text>
+      <Text style={{ fontWeight: "bold", fontSize: 26 }}>Log in</Text>
       <View style={{ marginTop: 40 }}>
         <TextInput
           style={styles.textInput}
